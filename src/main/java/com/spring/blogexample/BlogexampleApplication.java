4130d339edc94a2b43cac20bae533c16aae2533c
Jobson Ribeiro <jobsonrisoa@gmail.com>
2021-11-26 08:41:22 -0300

    Initial Setup

---
+++ src/main/java/com/spring/blogexample/BlogexampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.blogexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/blogexample/BlogexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.blogexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
