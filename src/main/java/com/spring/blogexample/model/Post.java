21375b4041668ae584b3b2f57b9970e8066cb578
Jobson Ribeiro <jobsonrisoa@gmail.com>
2021-11-26 11:43:14 -0300

    Dependencies javax.persistence and javax.contraints working!

---
+++ src/main/java/com/spring/blogexample/model/Post.java
@@ -0,0 +1,13 @@
+package com.spring.blogexample.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+
+@Entity
+@Table(name="TB_POST")
+public class Post {
+    
+
+}
