c2d790377aaa5c2d4207addc1c15972a89d9824f
Jobson Ribeiro <jobsonrisoa@gmail.com>
2021-11-28 12:12:18 -0300

    refreshing

--- src/main/java/com/spring/blogexample/controller/BlogexampleController.java
+++ src/main/java/com/spring/blogexample/controller/BlogexampleController.java
@@ -3,8 +3,9 @@ package com.spring.blogexample.controller;
 import com.spring.blogexample.model.Post;
 import com.spring.blogexample.service.BlogexampleService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -14,11 +15,11 @@ public class BlogexampleController {
 
     BlogexampleService blogexampleService;
 
-    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    @GetMapping("/post")
     public ModelAndView getPosts() {
-        ModelAndView mv = new ModelAndView("posts");
+        ModelAndView mv = new ModelAndView("post");
         List<Post> posts = blogexampleService.findAll();
-        mv.addObject("posts", posts);
+        mv.addObject("post", posts);
         return mv;
     }
 
