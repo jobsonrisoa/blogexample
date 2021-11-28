e6fce6b555a41041688f7ff851070797f0c3df70
Jobson Ribeiro <jobsonrisoa@gmail.com>
2021-11-28 14:24:31 -0300

    Update BlogexampleController.java

--- src/main/java/com/spring/blogexample/controller/BlogexampleController.java
+++ src/main/java/com/spring/blogexample/controller/BlogexampleController.java
@@ -2,10 +2,11 @@ package com.spring.blogexample.controller;
 
 import com.spring.blogexample.model.Post;
 import com.spring.blogexample.service.BlogexampleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @Controller
 public class BlogexampleController {
 
+    @Autowired
     BlogexampleService blogexampleService;
 
     @GetMapping("/post")
